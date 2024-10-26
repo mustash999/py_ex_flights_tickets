2f2d51535c30d80f351be7f1110b86b86b4f6241
Mustash <Mustash999@gmail.com>
2024-10-27 01:08:05 +0200

    update_notification_manager

--- srcs/notification_manager.py
+++ srcs/notification_manager.py
@@ -5,14 +5,18 @@ import						os
 class NotificationManager:
 	def __init__(self):
 		load_dotenv()
-		account_sid =	os.getenv('TWILIO_ACCOUNT_SID')
+		account_sid =	os.getenv('TWILIO_SID')
 		auth_token =	os.getenv('TWILIO_TOKEN')
 		self.client = 	Client(account_sid, auth_token)
 	
 	def send_sms(self, bodytext):
 		message = self.client.messages.create(
-			from_=os.getenv('TWILIO_PHONE_NUMBER'),
+			from_=os.getenv('TWILIO_NUM'),
 			body=bodytext,
-			to=os.getenv('MY_PHONE_NUMBER')
+			to=os.getenv('MY_NUM'))
 		print(message.sid)
 
+
+
+send = NotificationManager()
+send.send_sms("Hello, this is a test message from the Notification Manager.")
